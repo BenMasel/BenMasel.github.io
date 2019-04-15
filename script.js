29f758795d8fd894cb92eb5b1e672b7508855bda
Ben Masel <masel.benjamin@gmail.com>
2019-04-15 17:28:54 +0800

    Added Files

---
+++ script.js
@@ -0,0 +1,7 @@
+function getAge(d1, d2){
+    d2 = d2 || new Date();
+    var diff = d2.getTime() - d1.getTime();
+    return Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
+}
+console.log(getAge(new Date(2005, 9, 28)));
+document.getElementById("myAge").innerHTML = "I am a " + getAge(new Date(2005, 9, 28)) + " year old guy who loves programming, gaming and anything to do with computers!";
