76f826c4ce26879abc0e0ff4cfaca4ff47ddd195
Ben Masel <masel.benjamin@gmail.com>
2019-09-23 18:03:27 +0800

    Removed Personal Info And Added Stability

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ window.sr = ScrollReveal();
 sr.reveal('.anim');
 
 // Age Calulator
-function calculate_age(dob) {
-    var diff_ms = Date.now() - dob.getTime();
-    var age_dt = new Date(diff_ms);
+// function calculate_age(dob) {
+//    var diff_ms = Date.now() - dob.getTime();
+//    var age_dt = new Date(diff_ms);
+//
+//    return Math.abs(age_dt.getUTCFullYear() - 1970);
+//}
 
-    return Math.abs(age_dt.getUTCFullYear() - 1970);
+// document.getElementById('age').innerHTML = (calculate_age(new Date(0,0,0)));
+
+document.getElementById('say-hello-mailto').addEventListener('click', alert);
+document.getElementById('footer-mailto').addEventListener('click', alert);
+
+function alert() {
+ alert("Email Currently Not Available");
 }
-
-document.getElementById('age').innerHTML = (calculate_age(new Date(2005, 9, 28)));
