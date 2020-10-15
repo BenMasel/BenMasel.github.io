28c000bd8989df7ca5d7d5fd48f778329fe2475c
Ventus <46915278+BenMasel@users.noreply.github.com>
2020-10-15 18:17:54 +0800

    New Website Added

--- script.js
+++ script.js
@@ -1,35 +1,11 @@
-$('#read-more-hidden').addClass('hide');
+var line = document.getElementById("line");
+line.classList.add("widen");
 
-// Scroll Animation If Class = anim
-window.sr = ScrollReveal();
-sr.reveal('.anim');
-
-$(function() {
-  $(window).scroll(function() {
-    var scroll = $(window).scrollTop(); // how many pixels you've scrolled
-    var os = $('#profile').offset().top; // pixels to the top of div1
-    var ht = $('#profile').height(); // height of div1 in pixels
-    // if you've scrolled further than the top of div1 plus it's height
-    // change the color. either by adding a class or setting a css property
-    if (scroll > os + ht) {
-      $('#install').addClass('poop');
-    }
-  });
-});
-
-// Age Calulator
-// function calculate_age(dob) {
-//    var diff_ms = Date.now() - dob.getTime();
-//    var age_dt = new Date(diff_ms);
+// const [red, green, blue] = [255, 255, 255]
+// const section1 = document.querySelector('.body')
 //
-//    return Math.abs(age_dt.getUTCFullYear() - 1970);
-//}
-
-// document.getElementById('age').innerHTML = (calculate_age(new Date(0,0,0)));
-
-document.getElementById('read-more-work-experience').addEventListener('click', expandWorkExperience);
-
-function expandWorkExperience() {
-  $('#read-more-hidden').removeClass('hide');
-  $('#read-more-hidden').removeClass('show');
-}
+// window.addEventListener('scroll', () => {
+//   const y = 1 + (window.scrollY || window.pageYOffset) / 150
+//   const [r, g, b] = [red/y, green/y, blue/y].map(Math.round)
+//   section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+// })
