8a85cfb51d00b9a0f116d3edbf435c9a939ac8ff
Ventus <46915278+BenMasel@users.noreply.github.com>
2020-11-03 17:47:39 +0800

    Removed un-needed lines

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
 var line = document.getElementById("line");
 line.classList.add("widen");
-
-// const [red, green, blue] = [255, 255, 255]
-// const section1 = document.querySelector('.body')
-//
-// window.addEventListener('scroll', () => {
-//   const y = 1 + (window.scrollY || window.pageYOffset) / 150
-//   const [r, g, b] = [red/y, green/y, blue/y].map(Math.round)
-//   section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
-// })
