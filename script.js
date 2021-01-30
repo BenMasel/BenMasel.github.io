42376606ec9fbecce66a6fa3045b1c64be886fab
Ventus <46915278+BenMasel@users.noreply.github.com>
2021-01-30 14:51:38 +0800

    Formatted

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 var line = document.getElementById("line");
 line.classList.add("widen");
 
-var button = document.getElementById('resumeDownload');
-button.addEventListener('click', check);
+var button = document.getElementById("resumeDownload");
+button.addEventListener("click", check);
 
 function check() {
-    button.innerHTML = "✔️";
-    button.classList.add("button2active");
-    setTimeout(unCheck, 3000);
+  button.innerHTML = "✔️";
+  button.classList.add("button2active");
+  setTimeout(unCheck, 3000);
 }
 
 function unCheck() {
-    button.classList.remove("button2active");
-    button.innerHTML = "Résumé";
+  button.classList.remove("button2active");
+  button.innerHTML = "Résumé";
 }
